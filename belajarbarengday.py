8e48aee4be3e45cd043dd3f729397b3ee52d3a4c
xyzjgmday <daysubhiz@gmail.com>
2023-10-16 00:37:19 +0700

    rekursif

--- belajarbarengday.py
+++ belajarbarengday.py
@@ -1,14 +1,30 @@
 def hitungKecepatan():
     print("Hitung kecepatan gass")
-    jarak = float(input("Masukkin jaraknya berapa : "))
-    waktu = float(input("Masukkin waktunya berapa : "))
-    kecepatan = jarak * waktu
+    jarak = float(input("Masukkan jaraknya berapa: "))
+    waktu = float(input("Masukkan waktunya berapa: "))
+    kecepatan = jarak / waktu
     print("Kecepatan: ", kecepatan)
+    input("Tekan 'Enter' untuk melanjutkan...")
 
-    while True:
-        userInput = input(
-            "Pilih rumus yang akan dipakai \n\n1. Hitung Kecepatan\n2. Luas Segitiga\n3. Luas Persegi\n4. Luas Lingkaran\n\nMau pilih yang mana:  ")
-        if(userInput == 1):
-            hitungKecepatan()
-        else:
-            break
+while True:
+    print("\nPilih rumus yang akan dipakai:\n")
+    print("1. Hitung Kecepatan")
+    print("2. Luas Segitiga")
+    print("3. Luas Persegi")
+    print("4. Luas Lingkaran")
+    
+    userInput = input("\nMau pilih yang mana (masukkan nomor): ")
+    
+    if userInput == "1":
+        hitungKecepatan()
+    elif userInput == "2":
+        # Tambahkan kode untuk menghitung luas segitiga di sini
+        pass
+    elif userInput == "3":
+        # Tambahkan kode untuk menghitung luas persegi di sini
+        pass
+    elif userInput == "4":
+        # Tambahkan kode untuk menghitung luas lingkaran di sini
+        pass
+    else:
+        break
