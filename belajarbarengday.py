440dbc0c4dc76b1aaf1f3f624a088b6dacb9cc0b
xyzjgmday <daysubhiz@gmail.com>
2023-10-16 00:25:31 +0700

    rumus kecepatan

--- belajarbarengday.py
+++ belajarbarengday.py
@@ -1,1 +1,8 @@
-print('hehehe')
+def hitungKecepatan():
+    print("Hitung kecepatan gass")
+    jarak = float(input("Masukkin jaraknya berapa : "))
+    waktu = float(input("Masukkin waktunya berapa : "))
+    kecepatan = jarak * waktu
+    print("Kecepatan: ", kecepatan)
+
+hitungKecepatan()
